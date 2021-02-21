dc8a1c91286bee308fd3d941083f9891bdeec20e
riskanursafitri <safitririska010@gmail.com>
2021-02-21 10:04:25 +0700

    praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,6 @@ use App\Http\Controllers\ArticleController;
 
 //Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/about', [AboutController::class, 'about']);
+//Route::get('/about', [AboutController::class, 'about']);
+
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
