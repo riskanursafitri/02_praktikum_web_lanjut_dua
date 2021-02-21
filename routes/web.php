ce3996100fa5f80444630d7b1a2f7cc511e0d91f
riskanursafitri <safitririska010@gmail.com>
2021-02-21 09:36:49 +0700

    praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
