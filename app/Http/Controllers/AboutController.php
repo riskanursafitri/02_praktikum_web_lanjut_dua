9866f64031758e09c5dd64d12bf762ef744fcc8b
riskanursafitri <safitririska010@gmail.com>
2021-02-21 09:59:15 +0700

    praktikum 2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(){
+        return "Riska Nur Safitri 1941720073";
+    }
+}
